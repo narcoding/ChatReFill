56f9bfd4d110e602cc0c7da47bfe2abfc5c929a9
narcoding <narcoding@gmail.com>
2016-09-30 03:33:36 +0300

    login düzeltilecek

--- app/src/main/java/com/narcoding/chaturta/SigninActivity.java
+++ app/src/main/java/com/narcoding/chaturta/SigninActivity.java
@@ -16,19 +16,65 @@ import java.util.List;
 
 import io.socket.client.Socket;
 
-public class SigninActivity extends Activity {
+public class SigninActivity extends Activity implements View.OnClickListener {
 
-    private EditText mUserMailView;
+    private EditText edittext_edumail;
+    private EditText edittext_username;
+    private EditText edittext_password;
+
+    private String str_edumail;
+    private String str_username;
+
+    private Button btn_sign_up;
+    private Button btn_log_in;
+    private Button btn_sign_up_mail;
+    private Button btn_log_in_username;
+    private Button btn_back;
 
     private TextView text;
-
-
-    private Button signInMailButton;
-    private Button logInNickButton;
-
-    private String mUserMail;
+    private TextView txt_or;
 
     private Socket mSocket;
+
+    private void init(){
+
+        btn_sign_up= (Button) findViewById(R.id.btn_sign_up);
+        btn_log_in= (Button) findViewById(R.id.btn_log_in);
+        btn_sign_up_mail= (Button) findViewById(R.id.btn_sign_up_mail);
+        btn_log_in_username= (Button) findViewById(R.id.btn_log_in_username);
+        btn_back= (Button) findViewById(R.id.btn_back);
+
+        btn_sign_up.setOnClickListener((View.OnClickListener)this);
+        btn_log_in.setOnClickListener((View.OnClickListener)this);
+        btn_sign_up_mail.setOnClickListener((View.OnClickListener)this);
+        btn_log_in_username.setOnClickListener((View.OnClickListener)this);
+        btn_back.setOnClickListener((View.OnClickListener)this);
+
+        text= (TextView) findViewById(R.id.textView);
+        txt_or= (TextView) findViewById(R.id.txt_or);
+
+        edittext_edumail= (EditText) findViewById(R.id.edittext_edumail);
+        edittext_username= (EditText) findViewById(R.id.edittext_username);
+        edittext_password= (EditText) findViewById(R.id.edittext_password);
+
+    }
+
+    //@Override
+    //public void onBackPressed() {
+    //    super.onBackPressed();
+//
+    //    btn_sign_up_mail.setVisibility(View.GONE);
+    //    edittext_edumail.setVisibility(View.GONE);
+    //    edittext_username.setVisibility(View.GONE);
+    //    edittext_password.setVisibility(View.GONE);
+    //    text.setVisibility(View.GONE);
+    //    btn_log_in.setVisibility(View.VISIBLE);
+//
+    //}
+
+
+
+
 
 
     @Override
@@ -36,54 +82,68 @@ public class SigninActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-        mUserMailView= (EditText) findViewById(R.id.edumail_input);
+        init();
 
-        text= (TextView) findViewById(R.id.textView);
+    }
 
 
-        signInMailButton = (Button) findViewById(R.id.sign_in_mail);
-        signInMailButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btn_sign_up:
+                edittext_edumail.setVisibility(View.VISIBLE);
+                edittext_username.setVisibility(View.VISIBLE);
+                btn_sign_up_mail.setVisibility(View.VISIBLE);
+                btn_sign_up.setVisibility(View.GONE);
+                btn_log_in.setVisibility(View.GONE);
+                txt_or.setVisibility(View.GONE);
+                break;
+
+            case R.id.btn_log_in:
+                edittext_username.setVisibility(View.VISIBLE);
+                edittext_password.setVisibility(View.VISIBLE);
+                btn_log_in_username.setVisibility(View.VISIBLE);
+                btn_sign_up.setVisibility(View.GONE);
+                txt_or.setVisibility(View.GONE);
+                btn_log_in.setVisibility(View.GONE);
+                break;
+
+            case R.id.btn_sign_up_mail:
                 sendMailTo();
-            }
-        });
+                edittext_edumail.setVisibility(View.GONE);
+                edittext_username.setVisibility(View.GONE);
+                btn_sign_up_mail.setVisibility(View.GONE);
+                btn_back.setVisibility(View.VISIBLE);
+                text.setVisibility(View.VISIBLE);
+                break;
 
+            case R.id.btn_back:
+                btn_sign_up.setVisibility(View.VISIBLE);
+                txt_or.setVisibility(View.VISIBLE);
+                btn_log_in.setVisibility(View.VISIBLE);
+                btn_back.setVisibility(View.GONE);
+                text.setVisibility(View.GONE);
+                break;
 
-        logInNickButton= (Button) findViewById(R.id.log_in_nick);
-        logInNickButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
+            case R.id.btn_log_in_username:
                 Intent mIntent = new Intent();
                 mIntent.setClass(SigninActivity.this, MainActivity.class);
                 startActivity(mIntent);
                 finish();
-
-            }
-        });
-
-
-
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-
+                break;
+        }
     }
 
 
+public final class SessionIdentifieerGenerator {
+    private SecureRandom random = new SecureRandom();
 
-    public final class SessionIdentifieerGenerator{
-        private SecureRandom random=new SecureRandom();
-        public String nextSessionId(){
-          return new BigInteger(5,random).toString();
-        };
-    }
-
+    public String nextSessionId() {
+        return new BigInteger(5, random).toString();
+    };
+}
 
     private void sendMailTo(){
 
@@ -91,7 +151,7 @@ public class SigninActivity extends Activity {
 
         String fromEmail = "chatrefill.noreply@gmail.com";//((TextView) findViewById(R.id.editText1)).getText().toString();
         String fromPassword = "selamlar";//((TextView) findViewById(R.id.editText2)).getText().toString();
-        String toEmails = mUserMailView.getText().toString().trim();
+        String toEmails = edittext_edumail.getText().toString().trim();
         List<String> toEmailList = Arrays.asList(toEmails
                 .split("\\s*,\\s*"));
         Log.i("SendMailActivity", "To List: " + toEmailList);
@@ -101,11 +161,10 @@ public class SigninActivity extends Activity {
                 fromPassword, toEmailList, emailSubject, emailBody);
 
 
-        text.setVisibility(View.VISIBLE);
-        signInMailButton.setVisibility(View.GONE);
-        logInNickButton.setVisibility(View.GONE);
-
     }
 
 
+
+
 }
+
