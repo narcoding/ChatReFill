48ef5947a6406105f6aca0a3ed40af1d5e766d32
narcoding <narcoding@gmail.com>
2016-10-16 20:12:57 +0300

    profil şeyleri

--- app/src/main/java/com/narcoding/chaturta/Profil.java
+++ app/src/main/java/com/narcoding/chaturta/Profil.java
@@ -4,7 +4,10 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
@@ -14,14 +17,18 @@ public class Profil extends ActionBarActivity {
     SharedPreferences mPrefs;
 
     TextView txt_profile_username;
+    ImageButton btn_profile_image;
 
     private void init(){
+
         mPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         Gson gson = new Gson();
         kullanici = gson.fromJson(mPrefs.getString("kullanici", ""), Kullanici.class);
 
         txt_profile_username= (TextView) findViewById(R.id.txt_profile_username);
         txt_profile_username.setText(kullanici.KullaniciAdi);
+
+        btn_profile_image= (ImageButton) findViewById(R.id.btn_profile_image);
     }
 
     @Override
@@ -30,6 +37,13 @@ public class Profil extends ActionBarActivity {
         setContentView(R.layout.activity_profil);
 
         init();
+
+        btn_profile_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Profil Resmi Yükleyin",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
